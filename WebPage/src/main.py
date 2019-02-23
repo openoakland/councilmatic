2bb873737a415e17be3870dbcc63da69a62f1827
Howard Matis <HSMatis@gmail.com>
2019-02-23 14:25:59 -0800

    My IDE complained about the formatting of some of the lines.  I just made cosmetic changes so I would not see the error messages.

--- WebPage/src/main.py
+++ WebPage/src/main.py
@@ -11,20 +11,19 @@ COMMITTEES = ["City Council", "Rules & Legislation", "Public Works", "Life Enric
               "Oakland Redevelopment", "Community & Economic Development", "Finance & Management"]
 
 
-def committee_name_to_url(committee_name):
-    ''' e.g. "Rules & Legislation" -> 'rules-and-legislation' '''
+def committee_name_to_url(committee_name):  # e.g. "Rules & Legislation" -> 'rules-and-legislation'
     return re.sub(r'[^a-z]+', '-', committee_name.lower().replace('&', 'and'))
 
 
 def load_meetings(scraped_data, committee_name_filter=None, upcoming_only=False, skip_cancellations=False):
-    '''
+    """
     given a parsed CSV (scraped_data), returns a dict that can be used in the
     sidebar and main content area that looks like:
 
     {
         (date): [{meeting row dict}, {meeting row dict}, ...]
     }
-    '''
+    """
 
     today = datetime.now()
     midnight = datetime.combine(today, datetime.min.time())
@@ -100,23 +99,22 @@ current_directory = os.path.abspath(os.path.dirname(__file__))
 # first.
 current_year = datetime.now().year
 scraped_file = os.path.join(current_directory, '../website/scraped/year{}.csv'.format(current_year))
-scraped_data = list(csv.DictReader(open(scraped_file, encoding="utf-8"),
-    delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True))
+scraped_data = list(csv.DictReader(open(scraped_file, encoding="utf-8"),delimiter=',', quotechar='"',
+                                   quoting=csv.QUOTE_ALL, skipinitialspace=True))
 sidebar_items = load_meetings(scraped_data, upcoming_only=True)
 
 # generate the pages for other years
 for year in YEARS:
     # load the CSV for the year
     scraped_file = os.path.join(current_directory, '../website/scraped/year{}.csv'.format(year))
-    scraped_data = list(csv.DictReader(open(scraped_file, encoding="utf-8"),
-        delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True))
+    scraped_data = list(csv.DictReader(open(scraped_file, encoding="utf-8"),delimiter=',', quotechar='"',
+                                       quoting=csv.QUOTE_ALL, skipinitialspace=True))
 
     # generate a page for each committee in that year
     for committee_name in COMMITTEES:
         slug = committee_name_to_url(committee_name)
         outfile = os.path.join(current_directory, '../website/{}/{}.html'.format(year, slug))
 
-        render_committee_page(outfile, committee_name, year,
-            sidebar_items=sidebar_items,
-            meetings=load_meetings(scraped_data, committee_name_filter=committee_name, skip_cancellations=True),
-        )
+        render_committee_page(outfile, committee_name, year, sidebar_items = sidebar_items,
+                              meetings=load_meetings(scraped_data, committee_name_filter=committee_name,
+                                                skip_cancellations=True),)   # Don't know what this comma is for - HSM
